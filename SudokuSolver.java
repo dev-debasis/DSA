de72b5b4ba994e2953dde0951bcbb73d38bd5ef2
Debasis Khamari <129365803+dev-debasis@users.noreply.github.com>
2024-05-11 20:05:55 +0530

    Uploading the file 'SudokuSolver'

---
+++ SudokuSolver.java
@@ -0,0 +1,81 @@
+import java.util.Arrays;
+
+public class Sudoku {
+    public static void main(String[] args) {
+        int[][] sudokuBoard = {
+                { 3, 0, 6, 5, 0, 8, 4, 0, 0 },
+                { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
+                { 0, 0, 3, 0, 1, 0, 0, 8, 0 },
+                { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
+                { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
+                { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
+                { 0, 0, 5, 2, 0, 6, 3, 0, 0 }
+        };
+        boolean isSolved = sudoku(sudokuBoard);
+        if (isSolved) {
+            display(sudokuBoard);
+        } else {
+            System.out.println("Can't find solution");
+        }
+    }
+
+    public static boolean sudoku(int[][] board) {
+
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board.length; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 0; num <= 9; num++) {
+                        if (isSafe(board, num, row, col)) {
+                            board[row][col] = num;
+                            if (sudoku(board)) {
+                                return true;
+                            } else {
+                                board[row][col] = 0;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static void display(int[][] board) {
+        for (int[] row : board) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+
+    private static boolean isSafe(int[][] board, int num, int row, int col) {
+        // Checking the Row
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] == num) {
+                return false;
+            }
+        }
+
+        // Checking the col
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+
+        // Checking the grid
+
+        int rowStart = (row / 3) * 3;
+        int colStart = (col / 3) * 3;
+        for (int i = rowStart; i < rowStart + 3; i++) {
+            for (int j = colStart; j < colStart + 3; j++) {
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
